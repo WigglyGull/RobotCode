1e072fa4b184a9f6518062411a04c1803a59c048
Auron <auronwalkeris@gmail.com>
2024-08-29 14:26:52 +1200

    Update robotCode.ino
    modular :)

--- robotCode.ino
+++ robotCode.ino
@@ -4,19 +4,34 @@
 UcTTDcMotor motorL(D3, D9); // Instantiate a motor driver with forward and reverse pins
 UcTTDcMotor motorR(D10, D11); // Instantiate a motor driver with forward and reverse pins
 
-int trigPin = 2;    // Trigger
-int echoPin = 3;    // Echo
+int trigPin = 6;    // Trigger
+int echoPin = 7;    // Echo
 long duration, cm, inches;
  
 void setup() {
   //Serial Port begin
   Serial.begin (9600);
+
   //Define inputs and outputs
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+
+  //Sets up motor
+  motorL.init();  // Starts PWM @ ~31kHz
+  motorR.init();  // Starts PWM @ ~31kHz
 }
  
 void loop() {
+  int distance = detectDistance;
+  moveForward(80);
+  if(distance <= 5){
+    motorL.stop();
+    motorR.stop();
+  }
+}
+
+//returns distance from object in front in centermeters
+int detectDistance(){
   // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
   // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
   digitalWrite(trigPin, LOW);
@@ -29,19 +44,10 @@ void loop() {
   // duration is the time (in microseconds) from the sending
   // of the ping to the reception of its echo off of an object.
   pinMode(echoPin, INPUT);
-  duration = pulseIn(echoPin, HIGH);
+  duration = ulseIn(echoPin, HIGH);
  
   // Convert the time into a distance
-  cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
-  inches = (duration/2) / 74;   // Divide by 74 or multiply by 0.0135
-  
-  Serial.print(inches);
-  Serial.print("in, ");
-  Serial.print(cm);
-  Serial.print("cm");
-  Serial.println();
-  
-  delay(250);
+  return (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
 }
 
 void moveForward(int speed){
