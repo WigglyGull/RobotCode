a23f4965fd87959772ac632d371baa5ce022d396
Auron Walker <awa158@uclive.ac.nz>
2024-09-05 14:30:16 +1200

    leds working

--- src/robotMovement.cpp
+++ src/robotMovement.cpp
@@ -17,6 +17,7 @@ const int ENCODER_PIN_R = 4;
 const int FORWARD_LED = 13;
 const int TURN_LED = 12;
 const int HAZARD_LED = 8;
+const int LEDS[3] = {FORWARD_LED, TURN_LED, HAZARD_LED};
 
 // Ratio between pulse return time and distance in cm
 const float DISTANCE_RATIO = 29.1;   
@@ -58,6 +59,9 @@ void setup() {
   //Define inputs and outputs
   pinMode(TRIGGER_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
+  pinMode(FORWARD_LED, OUTPUT);
+  pinMode(TURN_LED, OUTPUT);
+  pinMode(HAZARD_LED, OUTPUT);
 
   //Sets up motor
   motorL.init();  // Starts PWM @ ~31kHz
@@ -66,6 +70,11 @@ void setup() {
 
 bool hasStopped = false;
 void loop() {
+    //Turn leds off
+    for (int i = 0; i < 2; i++){
+        digitalWrite(LEDS[i], LOW);  
+    }
+
     //Stops and cancles all movement if sensors pick up an object within 5cm
     int distance = pollDistance();
     if(distance <= 10){
@@ -75,11 +84,11 @@ void loop() {
         }
         motorController.stop();
         Serial.read(); //stops it reading input and excuting it after the hazard is removed
+        digitalWrite(HAZARD_LED, HIGH); 
         return;
     }
     
     //Moves Arduino based on WASD
-    
     if (Serial.available() > 0) {
         char command = Serial.read();
         int speed = isUpperCase(command) ? 40 : 80;
@@ -87,12 +96,15 @@ void loop() {
 
         if (command == 'W' || command == 'w') {
             motorController.forward(speed);
+            digitalWrite(FORWARD_LED, HIGH); 
         } else if (command == 'S' || command == 's') {
             motorController.reverse(speed);
         } else if (command == 'A' || command == 'a') {
             motorController.turnRobotByDegrees(1);
+            digitalWrite(TURN_LED, HIGH); 
         } else if (command == 'D' || command == 'd') {
             motorController.turnRobotByDegrees(-1);
+            digitalWrite(TURN_LED, HIGH);
         } 
     } else {
         motorController.stop();
