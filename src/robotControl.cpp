c67b9ee55ecf1633f9f99c08bbb17f09e0998a84
Connor Varney <cva68@uclive.ac.nz>
2024-09-13 11:16:02 +1200

    Improved structure, further commenting

--- src/robotControl.cpp
+++ src/robotControl.cpp
@@ -29,20 +29,12 @@ const int FORWARD_LED = 13;
 const int TURN_LED = 12;
 const int HAZARD_LED = 8;
 
-const int LEDS[3] = {13, 12, 8};
-
-// Allocate global memory for storing each character received over serial
-size_t commandLength = 0;
-char buffer = -1;
-
-// Ratio between pulse return time and distance in cm
-const float DISTANCE_RATIO = 29.1;   
-
 // Define motor connections
 UcTTDcMotor motorL(MOTOR_PIN_LF, MOTOR_PIN_LR); 
 UcTTDcMotor motorR(MOTOR_PIN_RF, MOTOR_PIN_RR); 
+
+// Configure the dualMotorControl library with pointers to our motor definitions, encoders and LEDs
 DualMotorControl motorController(&motorL, &motorR, ENCODER_PIN_L, ENCODER_PIN_R); 
-long duration;
 
 
 class Interactions {
@@ -52,6 +44,11 @@ class Interactions {
     public:
         int pollDistance()
         {
+            /*
+            * Return the distance in centimetres to the closest obstacle
+            * Modified from https://randomnerdtutorials.com/complete-guide-for-ultrasonic-sensor-hc-sr04
+            */
+
             // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
             // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
             digitalWrite(TRIGGER_PIN, LOW);
@@ -72,6 +69,9 @@ class Interactions {
 
         void printDistance()
         {
+            /*
+            * Prints the distance to the nearest obstacle to the serial console 
+            */
             Serial.print("Nearest obstacle: ");
             Serial.print(pollDistance());
             Serial.print("cm\n\r");
@@ -79,6 +79,9 @@ class Interactions {
 
         void search() 
         {
+            /*
+            * Search mode - moves forward until it finds an obstacle
+            */
             motorController.forward(60);
             for(size_t i = 0; i < 200; i++) {
                 if (pollDistance() <= 10) {
@@ -91,9 +94,13 @@ class Interactions {
             Serial.print("Timed out finding obstacle. Stopping.\n\r");
             motorController.stop();
         }   
+    private:
+        // Ratio between pulse return time and distance in cm
+        const float DISTANCE_RATIO = 29.1; 
+        long duration;
+
 };
     
-
 
 class Control {
     /*
@@ -106,23 +113,32 @@ class Control {
             /*
             * Move the robot forward, constantly checking for obstacles
             */
-           if (currentCommand != 'w' && currentCommand != 'h') {
-                currentCommand = 'w';
-                motorController.forward(speed);
-                digitalWrite(FORWARD_LED, HIGH); 
-            }
 
+            // If there's an obstacle, stop and prevent the robot from moving.
             if(interactions.pollDistance() <= 10){
                 if (currentCommand == 'w') {
                     Serial.println("Hazard detected! Please reverse.\r\n");
                     currentCommand = 'h';
+                    digitalWrite(HAZARD_LED, 1);
                 }
                 motorController.stop();
-                digitalWrite(FORWARD_LED, HIGH); 
+                return;
             }
+
+            // If we're not currently stopped for an obstacle, can move forwards
+           if (currentCommand != 'w' && currentCommand != 'h') {
+                currentCommand = 'w';
+                motorController.forward(speed);
+            }
+
+           
         }
 
         void handleKeypress() {
+            /*
+            * Switch the state of the robot when a new key is pressed.
+            * Stores the last state and checks against current state to prevent repeated calls to motorController
+            */
             switch (tolower(buffer)) {
                 case 'w':
                     forwardSafely();
@@ -132,7 +148,6 @@ class Control {
                     if (currentCommand != 's') {
                         currentCommand = 's';
                         motorController.reverse(speed);
-                        digitalWrite(FORWARD_LED, HIGH); 
                     }
                     break;
 
@@ -140,7 +155,6 @@ class Control {
                     if (currentCommand != 'a') {
                         currentCommand = 'a';
                         motorController.turnLeft(speed);
-                        digitalWrite(TURN_LED, HIGH); 
                     }
                     break;
 
@@ -148,7 +162,6 @@ class Control {
                     if (currentCommand != 'd') {
                         currentCommand = 'd';
                         motorController.turnRight(speed);
-                        digitalWrite(TURN_LED, HIGH);
                     }
                     break;
 
@@ -168,7 +181,7 @@ class Control {
             // Explain the interface to the user on startup
             Serial.println("Interactive mode: \n- Use WASD to move\n- Hold shift to move slower\n-Press 'x' to exit\n\n\r");
             
-            // 
+            // While the user hasn't requested to exit, read a new key if available
             while (buffer != 'x') {
                 if ( Serial.available() ) {
                     noInputTime = 0;
@@ -180,28 +193,29 @@ class Control {
                 } else {
                     if (noInputTime > 20) {
                             currentCommand = 'n';
-                            motorController.stop();
-                            for (int i = 0; i <= 2; i++){
-                                digitalWrite(LEDS[i], LOW);     
-                            }
-
                         } else {
                             noInputTime++;
                         }
+                }
+
+                delay(SERIAL_POLL_DELAY);
             }
-
-
-
-                delay(5);
         }
-    }
 
     private:
-        uint8_t SERIAL_POLL_DELAY = 5; // This shouldn't need adjusting, want to poll as fast as possible
+        // Serial poll delay should be as short as possible. Gaps between inputs are handled by noInputTime.
+        uint8_t SERIAL_POLL_DELAY = 5;
+
+        // Speeds are set to minimum and maximum that the motors support (without stall), should not need adjustment
+        uint8_t FAST_SPEED = 100;
+        uint8_t SLOW_SPEED = 40;
+
         Interactions interactions;
         size_t noInputTime = 0;
         char currentCommand = -1;
         int8_t speed = 0;
+        char buffer = -1;
+        
         
 };
 
@@ -216,22 +230,18 @@ class CommandInterface {
             /*
             * Start the command line interface. A state machine to move to a new control state depending on user input.
             */
-            char currentCommand[32] = {0};
+            currentCommand[32] = {0};
             Serial.print(">");
             readToBuffer(currentCommand);
 
 
+            // Compare the array of chars we read with the known commands we have stored, and perform the action.
             if ( memcmp(&currentCommand, &helpCommand, 4) == 0 ) {
                 Serial.println("\nAvailable commands:");
                 Serial.println("    control - Start interactive control of the robot\r");
                 Serial.println("    move - Move the robot a specified distance\r");
                 Serial.println("    turn - Turn the robot a specified number of degrees\r");
                 Serial.println("    distance - Display the distance to the nearest obstacle in front of the robot\n\r");
-
-            } else if ( memcmp(&currentCommand, &distanceCommand, 8) == 0 ) {
-                Serial.print("Closest obstacle: ");
-                Serial.print(interactions.pollDistance());
-                Serial.println("cm\n\r");
 
             } else if ( memcmp(&currentCommand, &controlCommand, 7) == 0 ) {
                 control.start();
@@ -244,14 +254,17 @@ class CommandInterface {
 
             } else if ( memcmp(&currentCommand, &distanceCommand, 8) == 0 ) {
                 interactions.printDistance();
+
             } else if ( memcmp(&currentCommand, &stopCommand, 4) == 0 ) {
                 motorController.stop();
+
             } else if ( memcmp(&currentCommand, &searchCommand, 6) == 0 ) {
                 interactions.search();
             }
         }
 
     private:
+        // Store the known commands in memory to allow comparisons later
         const char helpCommand[5] = "help";
         const char controlCommand[8] = "control";
         const char moveCommand[5] = "move";
@@ -264,15 +277,16 @@ class CommandInterface {
         Control control;
 
         void readToBuffer(char* currentCommand) 
+        /*
+        * Read an array of chars from the serial interface into a buffer, ending when \n or \r is sent
+        */
         {
-            buffer = -1;
-            commandLength = 0;
-
             do {
-                while ( !Serial.available() ) {} 
-                buffer = Serial.read();
-                Serial.print(buffer);
+                while ( !Serial.available() ) {} // Wait until a command is available
+                buffer = Serial.read(); // Read the character
+                Serial.print(buffer); // Echo the character back to the serial user
 
+                // For each character we read, store it in the currentCommand buffer
                 if ( commandLength < 32 ) {
                     if ( buffer == 8 && commandLength > 0) {
                         commandLength--;
@@ -283,36 +297,48 @@ class CommandInterface {
                 }
 
             } 
+            // Break on newline or carriage return
             while (buffer != 10 && buffer != 13);
             currentCommand[commandLength] = '\0';
         }
 
+    private:
+        // Define the buffers to store the current char, current command and it's length
+        size_t commandLength = 0;
+        char buffer = -1;
+        char currentCommand[32] = {0};
+
+
 };
+
 
 void setup() 
 {
-  //Serial Port begin
-  Serial.begin (9600);
+    /*
+    * Run the required setup code to begin the robot
+    */
+    //Serial Port begin
+    Serial.begin (9600);
 
-  //Define inputs and outputs
-  pinMode(TRIGGER_PIN, OUTPUT);
-  pinMode(ECHO_PIN, INPUT);
-  pinMode(FORWARD_LED, OUTPUT);
-  pinMode(TURN_LED, OUTPUT);
-  pinMode(HAZARD_LED, OUTPUT);
+    // Define inputs and outputs
+    pinMode(TRIGGER_PIN, OUTPUT);
+    pinMode(ECHO_PIN, INPUT);
+    pinMode(FORWARD_LED, OUTPUT);
+    pinMode(TURN_LED, OUTPUT);
+    pinMode(HAZARD_LED, OUTPUT);
 
-  //Sets up motor
-  motorL.init();  // Starts PWM @ ~31kHz
-  motorR.init();  // Starts PWM @ ~31kHz
+    // Set up motors
+    motorL.init();  // Starts PWM @ ~31kHz
+    motorR.init();  // Starts PWM @ ~31kHz
 
-  CommandInterface commandInterface;
-  
-  Serial.print("Robot Connected! Type 'help' for a list of commands\n\n\r");
-  commandInterface.console();
+    CommandInterface commandInterface;
+    
+    Serial.print("Robot Connected! Type 'help' for a list of commands\n\n\r");
+    commandInterface.console();
   
 }
 
-
+// Unsure if this needs to remain defined, to be tested later today
 void loop() 
 {
 
